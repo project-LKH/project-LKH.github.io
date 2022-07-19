47a700c152b618c2c0cbbde244e07e4fda160ef8
project-LKH <100766750+project-LKH@users.noreply.github.com>
2022-07-19 20:10:35 +0200

    added projects, contact section and scroll to top button

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
+var topbtn= document.getElementById("topBtn");
+window.onscroll = function(){
+    scrollFunction()
+}
+function scrollFunction(){
+    if (document.body.scrollTop>20||document.documentElement.scrollTop>20){
+        topbtn.style.display="block";
+    }else{
+        topbtn.style.display= "none";
+    }
+}
+ function topFunction(){
+    document.body.scrollTop=0;
+    document.documentElement.scrollTop=0;
+
+ }
+
+
+
+
 // const card = document.querySelector(".card");
 // const skills = document.querySelector(".skills");
 // const about = document.querySelector(".about");

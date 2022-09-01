646cfb90b38b43cdb8565ff540a5ba553ac6a15e
project-LKH <100766750+project-LKH@users.noreply.github.com>
2022-09-01 17:54:29 +0200

    adjustments to nav-bar

--- script.js
+++ script.js
@@ -21,7 +21,14 @@ function scrollFunction(){
   function off() {
     document.getElementById("overlay").style.display = "none";
   }
-
+  function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
 
 
 // const card = document.querySelector(".card");

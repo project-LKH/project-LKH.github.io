e13d5b6ae2bfb7373406626a74b346f7a9463c84
project-LKH <100766750+project-LKH@users.noreply.github.com>
2023-07-04 13:43:06 +0200

    added , contact section and updates to desktop style

--- src/script.js
+++ src/script.js
@@ -1,3 +1,5 @@
+
+
 function toggleNav() {
   const nav = document.getElementById("nav-bar");
   if (nav.style.display === "none") {
@@ -6,3 +8,21 @@ function toggleNav() {
     nav.style.display = "none";
   }
 }
+
+
+// const topBtn= document.getElementById("topBtn");
+// window.onscroll = function(){
+//     scrollFunction()
+// }
+// function scrollFunction(){
+//     if (document.body.scrollTop>20||document.documentElement.scrollTop>20){
+//         topBtn.style.display= "block";
+//     }else{
+//         topBtn.style.display= "none";
+//     }
+// }
+//  function topFunction(){
+//     document.body.scrollTop=0;
+//     document.documentElement.scrollTop=0;
+
+//  }
